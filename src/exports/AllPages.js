a0a15fad66dcc9e46590077e637423ca0c8edeb1
Anurag Yadav <119931366+AnuragYadav1920@users.noreply.github.com>
2025-04-28 13:23:38 +0530

    Update AllPages.js

--- src/exports/AllPages.js
+++ src/exports/AllPages.js
@@ -1,10 +1,10 @@
-export { default as Home } from '../Pages/Home.jsx';
-export { default as About } from '../Pages/About.jsx';
-export { default as Contact } from '../Pages/Contact.jsx';
-export { default as Education } from '../Pages/Education.jsx';
-export { default as Skills } from '../Pages/Skills.jsx';
-export { default as Certification } from '../Pages/Certification.jsx';
-export { default as Projects } from '../Pages/Projects.jsx';
-export { default as Training } from '../Pages/Training.jsx';
-export {default as Experience} from "../Pages/Experience.jsx"
-export {default as NoPage} from "../Pages/Nopage.jsx"
+export { default as Home } from '../pages/Home.jsx';
+export { default as About } from '../pages/About.jsx';
+export { default as Contact } from '../pages/Contact.jsx';
+export { default as Education } from '../pages/Education.jsx';
+export { default as Skills } from '../pages/Skills.jsx';
+export { default as Certification } from '../pages/Certification.jsx';
+export { default as Projects } from '../pages/Projects.jsx';
+export { default as Training } from '../pages/Training.jsx';
+export {default as Experience} from "../pages/Experience.jsx"
+export {default as NoPage} from "../pages/Nopage.jsx"
